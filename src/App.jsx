35973b80070de0b5d033d3a89162b19fcf7c8fbb
Cadufc91 <fernandes.cadu@gmail.com>
2022-06-16 13:12:05 -0300

    Estrutura basica do projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <>
+      <div>App</div>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
